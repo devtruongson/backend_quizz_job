3caff375c9f9d3051f120215be880744f3323b2e
Nguyễn Trường Sơn <133557424+devtruongson@users.noreply.github.com>
2025-05-11 00:46:37 +0700

    update change

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,9 @@
-const name: string = 'Fstack Dev Team'
-console.info(name)
+import 'dotenv/config';
+import express, { Application } from 'express';
+
+const app: Application = express();
+const PORT: number = parseInt(process.env.PORT || '8080');
+
+app.listen(PORT, () => {
+    console.log('App Start Successfully With Port: ' + PORT);
+});
